9e9706cec577ea24dbdbb3039f7a321da2c5567a
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-04-26 22:04:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TitleBlock from './CV_project/Title_CV/title__cv';
+
+
+
+function App() {
+  return (
+    <div>
+      <TitleBlock/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CV_project/Title_CV/title__cv.js
@@ -0,0 +1,21 @@
+import './title__cv.css'
+
+function titleBlock() {
+    return(
+        <div className='title__block'>
+            <p>Title</p>
+        </div>
+    )
+}
+
+export default titleBlock
+
+// import './Component1.css'
+
+// function Component1() {
+//     return(
+//         <h1>Component1</h1>
+//     )
+// }
+
+// export default Component1
