22dbb2977f5771ac25ead53da5ec009db0383797
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-04-28 22:41:45 +0300

    finish working task react number 1

--- src/CV_project/Info__CV/cv_info.jsx
+++ src/CV_project/Info__CV/cv_info.jsx
@@ -1,13 +1,14 @@
 import './cv_info.css'
 import About from './about/about'
 import Expirience from './work__expirience/expirience'
-
+import Education from './education/education'
 
 function InfoCV() {
     return(
         <div className="info__block">
             <About/>
             <Expirience/>
+            <Education/>
         </div>
     )
 }
