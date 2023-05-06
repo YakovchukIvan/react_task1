25218a0ff530033e9497fa55b390a83fa9e2d2ca
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-05-06 22:10:22 +0300

    codding contact list

--- src/Pages/ContactList/ContactList.jsx
+++ src/Pages/ContactList/ContactList.jsx
@@ -5,7 +5,122 @@ const ContactList = () => {
         <>
             <div className="wrapper">
 
-            <ul className="contact__list">
+            <div className="contact__block">
+
+                <div className="contact__block-status">
+                    <ul className='contact__list-status'>
+                        <li className='contact__item-status'>Work</li>
+                        <li className='contact__item-status'>Family</li>
+                        <li className='contact__item-status'>Private</li>
+                        <li className='contact__item-status'>Friends</li>
+                    </ul>
+                </div>
+
+                <div className="contact__user-block">
+                    <div className="contact__user">
+                        <p>Name</p>
+                        <p>Phone</p>
+                        <p>Email</p>
+                        <p>Avatar</p>
+                        <p>Gender</p>
+                        <p>Status</p>
+                        <p>Favorite</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Name</p>
+                        <p>Phone</p>
+                        <p>Email</p>
+                        <p>Avatar</p>
+                        <p>Gender</p>
+                        <p>Status</p>
+                        <p>Favorite</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                    <div className="contact__user">
+                        <p>Ivan</p>
+                        <p>380971231415</p>
+                        <p>Labuda@gmail.com</p>
+                        <p>Avatar</p>
+                        <p>Men</p>
+                        <p>Work</p>
+                        <p>True</p>
+                    </div>
+                </div>
+
+            </div>
+
+            {/* <ul className="contact__list">
                 <li className='item__list'>
                     <p>Name</p>
                     <p>Phone</p>
@@ -42,7 +157,7 @@ const ContactList = () => {
                     <p>Status</p>
                     <p>Favorite</p>
                 </li>
-            </ul>
+            </ul> */}
 
             </div>
         </>
